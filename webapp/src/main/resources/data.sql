ae7f6add5ba5d848247b9acb1e90ad54dcf1362e
kelgrim <ekneyber@hotmail.com>
2019-11-18 20:47:46 +0100

    made changes to database: added table with some (test)data

--- webapp/src/main/resources/data.sql
+++ webapp/src/main/resources/data.sql
@@ -2,3 +2,15 @@ INSERT INTO TBL_EMPLOYEES (first_name, last_name, email) VALUES
   ('Lokesh Local', 'Gupta', 'abc@gmail.com'),
   ('Deja Local', 'Vu', 'xyz@email.com'),
   ('Caption Local', 'America', 'cap@marvel.com');
+
+INSERT INTO TBL_MESSAGES (SENDER_ID, RECIPIENT_ID, MESSAGE_TEXT)
+VALUES (1,3,'Lokesh sends message to Caption');
+
+INSERT INTO TBL_MESSAGES (SENDER_ID, RECIPIENT_ID, MESSAGE_TEXT)
+VALUES (2,3,'Deja sends message to Caption');
+
+INSERT INTO TBL_MESSAGES (SENDER_ID, RECIPIENT_ID, MESSAGE_TEXT)
+VALUES (3,1,'Caption sends message to Lokeshs');
+
+
+
