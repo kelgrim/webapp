20b1da16ae4a1b11838e7a8b4fe62a6636423cee
kelgrim <ekneyber@hotmail.com>
2019-11-20 12:27:29 +0100

    Implement #1 add all current exceptions to CustomGlobalExceptionHandler
    Really only required adding the exceptions to the
    CustomGlobalExceptionHandler

--- webapp/src/main/java/ekn/learning/webapp/exceptions/CustomGlobalExceptionHandler.java
+++ webapp/src/main/java/ekn/learning/webapp/exceptions/CustomGlobalExceptionHandler.java
@@ -1,29 +1,22 @@
 package ekn.learning.webapp.exceptions;
 
-import org.springframework.http.HttpHeaders;
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolationException;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	  @ExceptionHandler(UserNotFoundException.class)
+	  @ExceptionHandler({
+		  UserNotFoundException.class,
+		  MessageNotFoundException.class,
+		  MessageNotFoundForUserException.class,	  
+	  })
 	  public ResponseEntity<CustomErrorResponse> customHandleNotFound(Exception ex, WebRequest request) {
 
 	        CustomErrorResponse errors = new CustomErrorResponse();
@@ -32,15 +25,22 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
 	        errors.setStatus(HttpStatus.NOT_FOUND.value());
 
 	        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
-
 	    }
 	  
-	  @ExceptionHandler(UserInvalidArgumentsException.class)
-	  public void constraintViolationException (HttpServletResponse response) throws IOException{
-		  response.sendError(HttpStatus.BAD_REQUEST.value());
+	  @ExceptionHandler({
+		  UserInvalidArgumentsException.class,
+		  MessageInvalidArgumentsException.class,
+		  UserDeleteFromDbFailedException.class,
+		  UserUpdateFailedException.class,
+		  UserWriteToDbFailedException.class
+	  })
+	  public ResponseEntity<CustomErrorResponse> customBadArguments(Exception ex, WebRequest request){
+		  CustomErrorResponse errors = new CustomErrorResponse();
+	        errors.setTimestamp(LocalDateTime.now());
+	        errors.setError(ex.getMessage());
+	        errors.setStatus(HttpStatus.BAD_REQUEST.value());
+
+	        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	  }
 	  
-	  
-	  
-	
 }
